128d5eb580ed54f831d451f2866c87e60c2d0bae
Ernesto1996 <netito86@hotmail.com>
2018-11-19 21:02:50 -0500

    Update ruta.ts

--- src/app/model/ruta.ts
+++ src/app/model/ruta.ts
@@ -1,5 +1,15 @@
+import { CampusService } from './../campus.service';
+import { Campus } from "./campus";
+
 export class Ruta {
     id:number;
     nombre:string;
-    
+    campus_salida: {
+        id: number
+        nombre_salida: String
+    }
+    campus_llegada: {
+        id:number
+        nombre_llegada: string
+    }
 }
